b54aa6ffb635ee6ea676aea9e9796a29ecb5f42c
robin0556 <roobinxyz@gmail.com>
2025-04-10 13:40:36 +0530

    learn6

---
+++ learn6.js
@@ -0,0 +1,43 @@
+// Strings
+
+
+
+// const name="Robin"
+// const repo=50;
+
+
+// console.log(name + repo);
+
+
+// console.log(`Hello ${name},Your repo count is ${repo}`);
+
+
+
+
+const gameName=new String("BGMI-IN")
+
+
+console.log(gameName[0]);
+
+
+
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('I'));
+
+const newString=gameName.substring(0,4)
+console.log(newString);
+
+
+// trim
+// replace
+// slice 
+// includes
+// split
+
+
+
+
