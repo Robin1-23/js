9f936c5149f38652a93434268a31279603f2948e
robin0556 <roobinxyz@gmail.com>
2025-04-10 23:22:41 +0530

    learn7

---
+++ learn7.js
@@ -0,0 +1,85 @@
+
+// const score =400
+// const balance =new Number(400);
+
+
+
+// console.log(score);
+// console.log(balance);
+
+
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+
+// const num=100.254
+
+// console.log(num.toPrecision(4));
+
+// const rupees=100000000
+
+// console.log(rupees.toLocaleString('en-IN'));
+
+
+
+
+
+// ------------Maths-----------------------------------
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(10.25));
+
+// console.log(Math.ceil(4.2));
+
+// console.log(Math.floor(4.2));
+
+
+
+// console.log(Math.ceil(Math.random()*10)+1);
+
+// console.log(Math.min(2,3,5461,2,1));
+
+// console.log(Math.max(1,2,3,45));
+
+
+
+
+
+// const min=10;
+// const max=20;
+
+
+// console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+// ----------------------Date------------------------------------
+
+
+
+let myDate=new Date();
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
+ 
+console.log(typeof myDate)    // object
+
+
+console.log(myDate.getDate())
+console.log(myDate.getFullYear());
+console.log(myDate.getMinutes())
+
+
+
+
+
+
+
+
+
+
