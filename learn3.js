8fad8358c68571557cdf13b3df556a1767921b91
robin0556 <roobinxyz@gmail.com>
2025-04-10 12:08:04 +0530

    learn3 added

---
+++ learn3.js
@@ -0,0 +1,95 @@
+// conversion operations
+
+
+
+// let score="33"
+
+// console.log(typeof score);
+
+
+// let scoreInNumber=Number(score);
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+
+
+
+
+// let a=null
+
+// console.log(typeof a);
+
+
+// let aInBool=Boolean(a)
+// console.log(aInBool);
+
+
+
+
+
+
+// "33"=>33
+// "33abcn"=>NaN
+// true=1 // false=0  ande vice-versa
+
+// ""=>false
+// "robin"=>true
+
+
+
+
+// *****************operations*******************
+
+
+
+// let val=3
+// let negval=-val
+// console.log(negval);
+
+
+
+// let str1="Hello"
+// let str2=" World"
+
+// console.log(str1+str2);
+
+
+
+
+// console.log("1"+2);    // 12
+// console.log(1+"1");    //11
+// console.log("2"+1+1);   //211
+// console.log(1+1+"2");   //22
+
+
+
+// console.log(+true);  // 1
+
+// // console.log(true++); // error
+
+// console.log(+"");  // 0
+
+
+
+let gameCounter=100
+
+gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
