c0c3a890a2a280d0a44651cfa849969c13fa6c94
robin0556 <roobinxyz@gmail.com>
2025-04-10 11:49:16 +0530

    Initial commit

---
+++ learn1.js
@@ -0,0 +1,66 @@
+console.log("Robin");
+
+
+// if(true){
+
+//     var name = "John";
+//     let age=10
+// }
+
+
+// console.log(name);
+// console.log(age);
+
+
+
+// 1
+
+// var a = 10;
+// var a = 20; // ✅ allowed
+
+// let b = 30;
+// let b = 40; // ❌ SyntaxError
+
+
+// 2
+
+// console.log(x); // undefined (var)
+// var x = 5;
+
+// console.log(y); // ❌ ReferenceError
+// let y = 10;
+
+// 3
+
+// if (true) {
+//     var a = 10;
+//     let b = 20;
+//   }
+//   console.log(a); // ✅ chalega
+//   console.log(b); // ❌ error dega
+
+
+// 4.
+
+// var: Purane JavaScript code mein use hota tha. Ab modern JS mein avoid karte hain.
+
+// let: Modern JavaScript mein recommended hai. Secure aur predictable hota hai.
+
+
+
+
+
+let a;
+
+let acountId=100000;
+
+let name="Robin"
+
+
+
+console.table([acountId,name,a])
+  
+
+
+
+
---
+++ learn2.js
@@ -0,0 +1,27 @@
+"use strict";    //treat all js code as newer version
+
+
+
+// alert(2+3)  // we are using node.js  it will only works in web browser
+
+
+// number      => 2 to the power 53
+// Boolean     =>true / false
+// BigInt
+// String =>"robin"
+// null    => standalone value
+// undefined =>
+//  symbol  =>unique
+
+
+
+
+// object
+
+
+
+console.log(typeof "Robin");         // string
+console.log(typeof null);           // object
+console.log(typeof undefined);     // undefined
+
+
