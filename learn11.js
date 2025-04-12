aeb33fa668b3052252c782819afa776eeea258ee
robin0556 <roobinxyz@gmail.com>
2025-04-12 12:15:11 +0530

    learnupdated11

--- learn11.js
+++ learn11.js
@@ -36,10 +36,25 @@ jsUser.email="robin@gmail.com"
 // console.log(jsUser);
 
 
-jsUser.greet=function(){
-    console.log(`hello ${this.name}`)
-}
+// jsUser.greet=function(){
+//     console.log(`hello ${this.name}`)
+// }
 
-console.log(jsUser.greet());
+// console.log(jsUser.greet());
 
+
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+
+
+const obj3={...obj1,...obj2}
+console.log(obj3);
+
+
+const obj4={...obj1,...obj2,5:"e",6:"f"}
+console.log(obj4);
+
+const obj5=Object.assign({},obj1,obj2)
+console.log(obj5);
 
